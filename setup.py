61ae6f663d82abdf05c4077fbc5aa685c341568a
caseyrb <caseyrb@kaermorhen.net>
2022-07-29 22:20:13 -0500

    Auto-installing requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
+import pathlib
+
+import pkg_resources
 from setuptools import setup
+
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
 
 setup(
     name='pycom',
     version='1.1.0',
+    install_requires=install_requires,
     entry_points={
         'console_scripts': [
             'pycom=pycom.main:main'
